7257a4e9ba00eca5fc38b55b0b5f0319d601e75b
cindyxinyiwang <xwang29@nd.edu>
2015-10-02 09:55:27 -0400

    add generate word and words

--- markov.py
+++ markov.py
@@ -24,6 +24,68 @@ class markov:
 			model[final_gram] = [None]
 		return model
 
+	def build_model_word(self, n):
+		model = dict()
+		tokens = self.text.strip().split(" ")
+		if len(tokens) < n:
+			return model
+		for i in range(len(tokens) - n):
+			gram = tuple(tokens[i:i+n])
+			next_token = tokens[i+n]
+			if gram in model:
+				model[gram].append(next_token)
+			else:
+				model[gram] = [next_token]
+		final_gram = tuple(tokens[len(tokens)-n:])
+		if final_gram in model:
+			model[final_gram].append(None)
+		else:
+			model[final_gram] = [None]
+		self.model = model
+
+	def next_word(self, myStr):
+		""" generate the next word based on input myStr """
+		prevTokens = tuple(myStr.strip().split(" "))
+		n = len(prevTokens)
+		if n < 1:
+			return None
+		self.build_model_word(n)
+		next_token = prevTokens[0]
+		if prevTokens in self.model:
+			return random.choice(self.model[prevTokens])
+		else:
+			return None
+
+	def next_words(self, myStr, genLen):
+		""" generate a string with length genLen based on the input myStr """
+		prevTokens = tuple(myStr.strip().split(" "))
+		n = len(prevTokens)
+		if n < 1:
+			return None
+		self.build_model_word(n)
+		next_token = prevTokens[0]
+		output = self.generate(self.model, n, prevTokens, genLen)
+		return ' '.join(output)
+
+	def build_model_char(self, n):
+		model = dict()
+		tokens = self.text
+		if len(tokens) < n:
+			return model
+		for i in range(len(tokens) - n):
+			gram = tuple(tokens[i:i+n])
+			next_token = tokens[i+n]
+			if gram in model:
+				model[gram].append(next_token)
+			else:
+				model[gram] = [next_token]
+		final_gram = tuple(tokens[len(tokens)-n:])
+		if final_gram in model:
+			model[final_gram].append(None)
+		else:
+			model[final_gram] = [None]
+		self.model = model
+
 	def generate(self, model, n, seed=None, max_iterations=100):
 		if seed is None:
 			seed = random.choice(model.keys())
@@ -63,4 +125,5 @@ if __name__ == '__main__':
 	n = int(sys.argv[1])
 	filepath = sys.argv[2]
 	mk = markov(filepath)
-	print mk.word_level_gen(n)
+	"""print mk.word_level_gen(n)"""
+	print mk.next_words("Sometimes", 20)
